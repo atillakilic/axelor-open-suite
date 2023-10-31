6735104636674d25ae797bb4a88e1638a76918d5
vijivani <vikasjivani13@gmail.com>
2023-10-31 20:21:25 +0530

    CashManagement: Add Advance Report template

--- axelor-cash-management/src/main/java/com/axelor/apps/cash/management/db/repo/CashManagementRepo.java
+++ axelor-cash-management/src/main/java/com/axelor/apps/cash/management/db/repo/CashManagementRepo.java
@@ -17,8 +17,6 @@
  */
 package com.axelor.apps.cash.management.db.repo;
 
-import javax.persistence.PersistenceException;
-
 import com.axelor.apps.base.db.Company;
 import com.axelor.apps.base.db.repo.CompanyRepository;
 import com.axelor.apps.base.db.repo.SequenceRepository;
@@ -29,33 +27,34 @@ import com.axelor.exception.db.repo.TraceBackRepository;
 import com.axelor.exception.service.TraceBackService;
 import com.axelor.i18n.I18n;
 import com.axelor.inject.Beans;
+import javax.persistence.PersistenceException;
 
 public class CashManagementRepo extends CashManagementRepository {
 
   @Override
   public CashManagement save(CashManagement entity) {
-	    try {
-	    	Company company  = Beans.get(CompanyRepository.class).all().fetchOne();
-	        if (entity.getCashManagementSeq() == null) {
-	          String seq =
-	              Beans.get(SequenceService.class)
-	                  .getSequenceNumber(
-	                      SequenceRepository.CASH_MANAGEMENT_SEQUENCE,
-	                      company,
-	                      CashManagement.class,
-	                      "cashManagementSeq");
-	          if (seq == null) {
-	            throw new AxelorException(
-	                TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
-	                I18n.get("There is no sequence set for the Cash Management for the company"),
-	                company.getName());
-	          }
-	          entity.setCashManagementSeq(seq);
-	        }
-	        return super.save(entity);
-	      } catch (Exception e) {
-	        TraceBackService.traceExceptionFromSaveMethod(e);
-	        throw new PersistenceException(e.getMessage(), e);
-	      }
-	    }
+    try {
+      Company company = Beans.get(CompanyRepository.class).all().fetchOne();
+      if (entity.getCashManagementSeq() == null) {
+        String seq =
+            Beans.get(SequenceService.class)
+                .getSequenceNumber(
+                    SequenceRepository.CASH_MANAGEMENT_SEQUENCE,
+                    company,
+                    CashManagement.class,
+                    "cashManagementSeq");
+        if (seq == null) {
+          throw new AxelorException(
+              TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
+              I18n.get("There is no sequence set for the Cash Management for the company"),
+              company.getName());
+        }
+        entity.setCashManagementSeq(seq);
+      }
+      return super.save(entity);
+    } catch (Exception e) {
+      TraceBackService.traceExceptionFromSaveMethod(e);
+      throw new PersistenceException(e.getMessage(), e);
+    }
+  }
 }
---
+++ axelor-cash-management/src/main/java/com/axelor/apps/cash/management/web/AdvancePaymentCashController.java
@@ -0,0 +1,75 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2023 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.axelor.apps.cash.management.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.axelor.apps.ReportFactory;
+import com.axelor.apps.cash.management.db.AdvancePaymentCash;
+import com.axelor.apps.cash.management.db.AdvancePaymentCashLine;
+import com.axelor.apps.hr.db.EmployeeContractRu;
+import com.axelor.apps.hr.db.EmployeeRu;
+import com.axelor.apps.hr.db.EmployeeSalaryRu;
+import com.axelor.apps.hr.db.repo.EmployeeRuRepository;
+import com.axelor.exception.service.TraceBackService;
+import com.axelor.inject.Beans;
+import com.axelor.meta.schema.actions.ActionView;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.google.common.base.Joiner;
+import com.google.inject.Singleton;
+
+@Singleton
+public class AdvancePaymentCashController {
+
+	public void setEmployee(ActionRequest request, ActionResponse response) {
+		AdvancePaymentCash advancePaymentCash = request.getContext().asType(AdvancePaymentCash.class);
+
+		List<EmployeeRu> employeeList = new ArrayList<EmployeeRu>();
+
+		employeeList = Beans.get(EmployeeRuRepository.class).all()
+				.filter("self.projectTeam = ?", advancePaymentCash.getProjectTeam()).fetch();
+		List<AdvancePaymentCashLine> advancePaymentCashLineList = new ArrayList<AdvancePaymentCashLine>();
+		int count = 0;
+		for (EmployeeRu employee : employeeList) {
+			count ++;
+			AdvancePaymentCashLine advancePaymentCashLine = new AdvancePaymentCashLine();
+			advancePaymentCashLine.setEmployeeRu(employee);
+			advancePaymentCashLine.setSeqNumber(String.valueOf(count));
+			advancePaymentCashLineList.add(advancePaymentCashLine);
+		}
+		response.setValue("advancePaymentCashLine", advancePaymentCashLineList);
+	}
+	
+	public void printReport(ActionRequest request, ActionResponse response) {
+		AdvancePaymentCash advancePaymentCash = request.getContext().asType(AdvancePaymentCash.class);
+	    try {
+	      String fileLink =
+	          ReportFactory.createReport("AdvancePayment.rptdesign", "Advance Payment" + "-${date}")
+	              .addParam("id", advancePaymentCash.getId())
+	              .generate()
+	              .getFileLink();
+
+	      response.setView(ActionView.define("Advance Payment").add("html", fileLink).map());
+	    } catch (Exception e) {
+	      TraceBackService.trace(response, e);
+	    }
+	  
+	}
+}
